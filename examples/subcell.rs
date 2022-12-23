037c3cf4a9ef546bf34304b539ff667110c38eba
Funnisimo <scastette@yahoo.com>
2022-12-23 16:28:18 -0600

    subcell example uses transparency

--- examples/subcell.rs
+++ examples/subcell.rs
@@ -31,20 +31,26 @@ impl ScreenCreator for MyRoguelike {
 
 impl Screen for MyRoguelike {
     fn render(&mut self, app: &mut dyn AppContext) {
-        // image
-        self.subcell.buffer_mut().clear(true, true, true);
-        draw::subcell(self.subcell.buffer_mut()).blit(&self.skull, 0, 0, 0, 0, None, None);
-        self.subcell.render(app.gl());
-
         // text
         let buffer = self.con.buffer_mut();
-        buffer.fill(None, Some(RGBA::rgba(255, 0, 255, 255)), None);
+        buffer.fill(
+            Some('.' as u32),
+            Some(RGBA::rgba(255, 0, 255, 255)),
+            Some(RGBA::rgb(32, 32, 32)),
+        );
         draw::plain(buffer)
             .align(TextAlign::Center)
             .fg(RGBA::rgb(0, 128, 255))
             .print_lines(30, 10, "This is a 60x80 png\non a 30x40 console.");
 
         self.con.render(app.gl());
+
+        // image
+        self.subcell.buffer_mut().clear(true, true, true);
+        draw::subcell(self.subcell.buffer_mut())
+            .transparent(RGBA::rgba(0, 0, 0, 255))
+            .blit(&self.skull, 0, 0, 0, 0, None, None);
+        self.subcell.render(app.gl());
     }
 }
 
