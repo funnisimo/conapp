942ae176973508782cb5fad3c96914b25120c0f8
Funnisimo <scastette@yahoo.com>
2022-12-31 08:43:41 -0700

    added minimal example

---
+++ examples/minimal.rs
@@ -0,0 +1,41 @@
+use conapp::*;
+
+struct MainScreen {
+    con: Console,
+}
+
+impl ScreenCreator for MainScreen {
+    fn create(app: &mut AppContext) -> Box<dyn Screen> {
+        let con = app
+            .simple_console(80, 50, "resources/terminal_8x8.png")
+            .expect("Failed to load font");
+        Box::new(MainScreen { con })
+    }
+}
+
+impl Screen for MainScreen {
+    fn input(&mut self, _app: &mut AppContext, ev: &AppEvent) -> ScreenResult {
+        match ev {
+            AppEvent::KeyDown(_) => ScreenResult::Quit,
+            AppEvent::MouseDown(_) => ScreenResult::Quit,
+            _ => ScreenResult::Continue,
+        }
+    }
+
+    fn render(&mut self, app: &mut AppContext) {
+        let buffer = self.con.buffer_mut();
+
+        buffer.fill(Some('.' as u32), Some(WHITE), Some(BLACK));
+
+        draw::plain(buffer)
+            .fg(RGBA::rgb(255, 0, 255))
+            .print(10, 10, "Hello World");
+
+        self.con.render(app);
+    }
+}
+
+fn main() {
+    let app = AppBuilder::new(1024, 768).title("Minimal Example").build();
+    app.run::<MainScreen>();
+}
